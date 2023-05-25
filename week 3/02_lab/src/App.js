cb6f11deb9a859307649dbbe1dfaac8b3a7f3337
Sagar More <animsh.more@gmail.com>
2023-05-25 17:10:28 +0530

    week 3: lab - displaying images

---
+++ week 3/02_lab/src/App.js
@@ -0,0 +1,14 @@
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img
+        width = "100%"
+        alt="logo" src={ logo }/>
+    </div>
+  );
+}
+
+export default App;
